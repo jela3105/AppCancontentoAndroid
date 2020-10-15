1587ee9e2127c3d8688fcc82f4b67d1685f566ed
jela3105 <jela3105@gmail.com>
2020-10-14 22:33:43 -0500

    BottomNavigation its working

--- app/src/main/java/com/example/loginregisterapp/MainActivity.java
+++ app/src/main/java/com/example/loginregisterapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.loginregisterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
@@ -20,59 +25,19 @@ import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText username, password;
-    String u,p;
-    Button submit;
-    RequestParams params;
-    AsyncHttpClient client;
-    public static final String MYURL = "http://192.168.1.67:8084/Preciso/Cliente";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        username = (EditText)findViewById(R.id.username);
-        password = (EditText)findViewById(R.id.password);
-        submit = (Button)findViewById(R.id.submit_btn);
-
-        submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                u = username.getText().toString();
-                p = password.getText().toString();
-
-                params = new RequestParams();
-                params.put("corr_compara",u);
-                params.put("contra_compara",p);
-                params.put("accion","login");
-                params.put("place","app");
-
-                client = new AsyncHttpClient();
-                client.post(MYURL, params, new JsonHttpResponseHandler(){
-                    @Override
-                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                        super.onSuccess(statusCode, headers, response);
-                        Toast.makeText(MainActivity.this,"Submit Success f" + response,Toast.LENGTH_SHORT).show();
-                        Intent i = new Intent(MainActivity.this, LoginActivity.class);
-                        startActivity(i);
-                    }
-
-                    @Override
-                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                        super.onFailure(statusCode, headers, responseString, throwable);
-                        Toast.makeText(MainActivity.this,"Something Went Wrong f "+ statusCode + responseString ,Toast.LENGTH_SHORT).show();
-                        if(statusCode == 405){
-                            Intent i = new Intent(MainActivity.this, LoginActivity.class);
-                            startActivity(i);
-                        }
-                    }
-                    //Toast.makeText(MainActivity.this,"Something Went Wrong" ,Toast.LENGTH_SHORT).show();
-
-
-                });
-            }
-        });
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_servicios, R.id.navigation_perfil).build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
 
     }
 }
