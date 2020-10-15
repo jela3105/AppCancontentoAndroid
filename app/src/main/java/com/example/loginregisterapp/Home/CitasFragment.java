f7306a95859c41234a072192f40229eecc4f1882
jela3105 <jela3105@gmail.com>
2020-10-15 00:39:00 -0500

    Floating button of citas is working

--- app/src/main/java/com/example/loginregisterapp/Home/CitasFragment.java
+++ app/src/main/java/com/example/loginregisterapp/Home/CitasFragment.java
@@ -7,15 +7,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.loginregisterapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class CitasFragment extends Fragment {
+
+    View myFragment;
 
     public CitasFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,15 @@ public class CitasFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_citas, container, false);
+        myFragment = inflater.inflate(R.layout.fragment_citas, container, false);
+        FloatingActionButton fab = myFragment.findViewById(R.id.add);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "helloworld", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return myFragment;
     }
+
 }
