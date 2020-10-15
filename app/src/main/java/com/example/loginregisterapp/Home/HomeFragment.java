f64124b7420ef52691cb35b8fe4a421b1ce5b841
jela3105 <jela3105@gmail.com>
2020-10-14 23:33:27 -0500

    Tabs added in home fragment

--- app/src/main/java/com/example/loginregisterapp/Home/HomeFragment.java
+++ app/src/main/java/com/example/loginregisterapp/Home/HomeFragment.java
@@ -2,22 +2,32 @@ package com.example.loginregisterapp.Home;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.loginregisterapp.R;
+import com.example.loginregisterapp.SectionPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
 
 
 public class HomeFragment extends Fragment {
 
+    View myFragment;
+    ViewPager viewPager;
+    TabLayout tabLayout;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
+    public static HomeFragment getInstance()    {
+        return new HomeFragment();
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,7 +38,45 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        myFragment = inflater.inflate(R.layout.fragment_home, container, false);
+
+        viewPager = myFragment.findViewById(R.id.viewPagerHome);
+        tabLayout = myFragment.findViewById(R.id.tabLayoutHome);
+
+        return myFragment;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        setUpViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    private void setUpViewPager(ViewPager viewPager) {
+        SectionPagerAdapter adapter = new SectionPagerAdapter(getChildFragmentManager());
+
+        adapter.addFragment(new MascotasFragment(), "Mascotas");
+        adapter.addFragment(new CitasFragment(), "Citas");
+
+        viewPager.setAdapter(adapter);
     }
 }
