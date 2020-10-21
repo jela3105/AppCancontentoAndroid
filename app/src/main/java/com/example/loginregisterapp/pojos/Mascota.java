c7dc1013f9ba1b18ade8e9e87adc88507ed2a54a
jela3105 <jela3105@gmail.com>
2020-10-20 23:13:32 -0500

    create pojo mascota.java

--- app/src/main/java/com/example/loginregisterapp/pojos/Mascota.java
+++ app/src/main/java/com/example/loginregisterapp/pojos/Mascota.java
@@ -1,4 +1,37 @@
 package com.example.loginregisterapp.pojos;
 
 public class Mascota {
+    private int fotoMascota;
+    private String nombreMascota;
+    private String descripcion;
+
+    public Mascota(int fotoMascota, String nombreMascota, String descripcion) {
+        this.fotoMascota = fotoMascota;
+        this.nombreMascota = nombreMascota;
+        this.descripcion = descripcion;
+    }
+
+    public int getFotoMascota() {
+        return fotoMascota;
+    }
+
+    public void setFotoMascota(int fotoMascota) {
+        this.fotoMascota = fotoMascota;
+    }
+
+    public String getNombreMascota() {
+        return nombreMascota;
+    }
+
+    public void setNombreMascota(String nombreMascota) {
+        this.nombreMascota = nombreMascota;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
