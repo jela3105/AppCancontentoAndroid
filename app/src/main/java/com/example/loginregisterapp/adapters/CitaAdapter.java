2ee8cd97827f06f0bb1ed250ca14fd7d0fbf2d3d
jela3105 <jela3105@gmail.com>
2020-11-06 23:39:53 -0600

    Find elements by id CitaAdapter

--- app/src/main/java/com/example/loginregisterapp/adapters/CitaAdapter.java
+++ app/src/main/java/com/example/loginregisterapp/adapters/CitaAdapter.java
@@ -2,9 +2,12 @@ package com.example.loginregisterapp.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,12 +22,18 @@ public class CitaAdapter extends RecyclerView.Adapter<CitaAdapter.CitaViewHolder
     public static class CitaViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         Context context;
+        TextView nombreServicio, nombreMascota, descripcionCita;
+        ImageView fotoMascota, imagenStatus;
 
         public CitaViewHolder(@NonNull View itemView) {
             super(itemView);
             context = itemView.getContext();
             //encontramos por id en item_cita
-
+            nombreMascota = (TextView) itemView.findViewById(R.id.nombre_cita_mascota);
+            nombreServicio = (TextView) itemView.findViewById(R.id.nombre_cita_servicio);
+            descripcionCita = (TextView) itemView.findViewById(R.id.descripcion_cita);
+            fotoMascota = (ImageView) itemView.findViewById(R.id.foto_cita_mascota);
+            imagenStatus = (ImageView) itemView.findViewById(R.id.status_cita);
         }
 
         public void setOnClickListeners() {
