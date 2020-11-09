e72094ae1a811520215ff2fea82316d187546b1b
jela3105 <jela3105@gmail.com>
2020-11-08 18:06:07 -0600

    Template ServiciosAdapter

--- app/src/main/java/com/example/loginregisterapp/adapters/ServiciosAdapter.java
+++ app/src/main/java/com/example/loginregisterapp/adapters/ServiciosAdapter.java
@@ -1,4 +1,41 @@
 package com.example.loginregisterapp.adapters;
 
-public class ServiciosAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ServiciosAdapter  extends RecyclerView.Adapter<ServiciosAdapter.ServiciosViewHolder>{
+
+    public class ServiciosViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        public ServiciosViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+        }
+
+
+    }
+
+    @NonNull
+    @Override
+    public ServiciosViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ServiciosViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
 }
