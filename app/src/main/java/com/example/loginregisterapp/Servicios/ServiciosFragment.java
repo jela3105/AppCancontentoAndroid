57a7dedd2293db143e2e95d7c18e15dfea90051d
jela3105 <jela3105@gmail.com>
2020-11-08 22:56:30 -0600

    Servicios are displayed, but not in horizontal

--- app/src/main/java/com/example/loginregisterapp/Servicios/ServiciosFragment.java
+++ app/src/main/java/com/example/loginregisterapp/Servicios/ServiciosFragment.java
@@ -2,17 +2,29 @@ package com.example.loginregisterapp.Servicios;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.loginregisterapp.R;
+import com.example.loginregisterapp.adapters.MascotaAdapter;
+import com.example.loginregisterapp.adapters.ServiciosAdapter;
+import com.example.loginregisterapp.pojos.Servicio;
+
+import java.util.ArrayList;
 
 
 public class ServiciosFragment extends Fragment {
 
+    private View myFragment;
+    RecyclerView recyclerViewServicios;
+    ArrayList<Servicio> listaServicios;
     public ServiciosFragment() {
         // Required empty public constructor
     }
@@ -27,6 +39,25 @@ public class ServiciosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_servicios, container, false);
+        myFragment =  inflater.inflate(R.layout.fragment_servicios, container, false);
+        recyclerViewServicios =  myFragment.findViewById(R.id.recycler_servicios);
+        return myFragment;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        listaServicios = new ArrayList<>();
+        recyclerViewServicios.setLayoutManager(new LinearLayoutManager(getContext()));
+        llenarLista();
+        ServiciosAdapter serviciosAdapter = new ServiciosAdapter(listaServicios);
+        recyclerViewServicios.setAdapter(serviciosAdapter);
+    }
+
+    private void llenarLista() {
+        listaServicios.add(new Servicio());
+        listaServicios.add(new Servicio());
+        listaServicios.add(new Servicio());
+
     }
 }
--- app/src/main/java/com/example/loginregisterapp/adapters/ServiciosAdapter.java
+++ app/src/main/java/com/example/loginregisterapp/adapters/ServiciosAdapter.java
@@ -1,17 +1,33 @@
 package com.example.loginregisterapp.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.loginregisterapp.R;
+import com.example.loginregisterapp.pojos.Servicio;
+
+import java.util.ArrayList;
 
 public class ServiciosAdapter  extends RecyclerView.Adapter<ServiciosAdapter.ServiciosViewHolder>{
 
     public class ServiciosViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
+        Context context;
+        TextView nombreServicio;
         public ServiciosViewHolder(@NonNull View itemView) {
             super(itemView);
+            setOnClickListeners();
+            context = itemView.getContext();
+            nombreServicio = (TextView) itemView.findViewById(R.id.nombre_servicio);
+        }
+
+        public void setOnClickListeners(){
         }
 
         @Override
@@ -21,21 +37,28 @@ public class ServiciosAdapter  extends RecyclerView.Adapter<ServiciosAdapter.Ser
 
 
     }
+    ArrayList<Servicio> listaServicios;
+
+    public ServiciosAdapter(ArrayList<Servicio> listaServicios) {
+        this.listaServicios = listaServicios;
+    }
 
     @NonNull
     @Override
     public ServiciosViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_servicio,null,false);
+        return new ServiciosAdapter.ServiciosViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ServiciosViewHolder holder, int position) {
-
+        holder.nombreServicio.setText(listaServicios.get(position).getNombre());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listaServicios.size();
     }
+
 
 }
