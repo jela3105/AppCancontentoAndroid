9c0a975d7e5516809a5045923645cd5f3701287f
jela3105 <jela3105@gmail.com>
2020-12-08 23:47:52 -0600

    Add pet image in MascotaActivity

--- app/src/main/java/com/example/loginregisterapp/MascotaActivity.java
+++ app/src/main/java/com/example/loginregisterapp/MascotaActivity.java
@@ -11,7 +11,7 @@ public class MascotaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_editar_mascota);
+        setContentView(R.layout.activity_mascota);
 
         Bundle extras = getIntent().getExtras();
         if(extras!= null){
